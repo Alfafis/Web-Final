15c17e28df7ffaa3707d0dfb09a539a2926fe3f8
Alfafis <ctpmmg2009_101@hotmail.com>
2019-12-07 13:41:12 -0300

    w64

--- view/js/app.js
+++ view/js/app.js
@@ -52,11 +52,13 @@ window.onload = function(e) {
 			var table = document.querySelector('#tableAlterAluno');
 			var row = table.insertRow(-1);
 			var labelColuna = row.insertCell(0);
-			var nomeColuna = row.insertCell(1); 
-			var optionColuna = row.insertCell(2);
+			var nomeColuna = row.insertCell(1);
+			var cursoColuna = row.insertCell(2);
+			var optionColuna = row.insertCell(3);
 			labelColuna.innerHTML = '<span>&nbsp</span>';
 			nomeColuna.innerHTML = empresa.nome;
-			optionColuna.innerHTML = '<div class="form-group"><button>alterar</button><button>deletar</button></div>';
+			cursoColuna.innerHTML = empresa.curso;
+			optionColuna.innerHTML = '<div class="d-flex justify-content-around"><button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editModal"><i class="material-icons">create</i></button><button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="material-icons">delete</i></button></div>';
 		})
 	}).catch(error => console.error(error))
 	fetch(
@@ -69,10 +71,12 @@ window.onload = function(e) {
 			var row = table.insertRow(-1);
 			var labelColuna = row.insertCell(0);
 			var nomeColuna = row.insertCell(1); 
-			var optionColuna = row.insertCell(2);
+			var areaColuna = row.insertCell(2);
+			var optionColuna = row.insertCell(3);
 			labelColuna.innerHTML = '<span>&nbsp</span>';
 			nomeColuna.innerHTML = empresa.nome;
-			optionColuna.innerHTML = '<div class="form-group"><button>alterar</button><button>deletar</button></div>';
+			areaColuna.innerHTML = empresa.area;
+			optionColuna.innerHTML = '<div class="d-flex justify-content-around"><button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#editModal"><i class="material-icons">create</i></button><button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="material-icons">delete</i></button></div>';
 		})
 	}).catch(error => console.error(error))
 }
@@ -112,6 +116,55 @@ function postFormEmpresa() {
 	// console.log(JSON.stringify(data));
 	fetch('http://localhost/Web-Final/empresa', {
 		method: 'POST',
+		body: JSON.stringify(data)
+	})
+	.then((response) => {
+		if (response.ok) {
+			return response.json() 
+		} else {
+			return Promise.reject({ status: res.status, statusText: res.statusText });
+		}   
+
+	})
+	.then((data) => console.log(data))
+	.catch(err => console.log('Error message:', err.statusText));
+}
+
+function updateFormAluno() {
+	var form = document.querySelector('#alterarAluno');
+	var data = {};
+	data['nome'] = form.nome.value
+	data['cpf'] = form.cpf.value;
+	data['idade'] = form.idade.value;
+	data['curso'] = form.curso.value; 
+	data['sexo'] = form.sexo.value;
+	data['matricula'] = form.matricula.value;
+	// console.log(JSON.stringify(data));
+	fetch('http://localhost/Web-Final/aluno', {
+		method: 'PUT',
+		body: JSON.stringify(data)
+	})
+	.then((response) => {
+		if (response.ok) {
+			return response.json() 
+		} else {
+			return Promise.reject({ status: res.status, statusText: res.statusText });
+		}   
+
+	})
+	.then((data) => console.log(data))
+	.catch(err => console.log('Error message:', err.statusText));
+}
+
+function updateFormEmpresa() {
+	var form = document.querySelector('#alterarEmpresa');
+	var data = {};
+	data['nome'] = form.nome.value
+	data['cnpj'] = form.cnpj.value;
+	data['area'] = form.area.value;
+	// console.log(JSON.stringify(data));
+	fetch('http://localhost/Web-Final/empresa', {
+		method: 'PUT',
 		body: JSON.stringify(data)
 	})
 	.then((response) => {
